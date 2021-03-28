ee4091c2eb1d8ebf608f2fbe30f90f48ec056edb
Luuk Linders <lunkkun@gmail.com>
2021-03-28 15:41:45 +0200

    added camera rig

--- scenes/CameraRig.gd
+++ scenes/CameraRig.gd
@@ -1,13 +1,14 @@
-extends Camera
+extends Spatial
 
 
 export(NodePath) var follow
 
 onready var _object_to_follow = get_node(follow) if follow else null
+onready var _camera = $Camera
 
 
 func _physics_process(delta):
 	if (follow):
 		var target = _object_to_follow.global_transform.origin
 		target.y += 1
-		look_at(target, Vector3.UP)
+		_camera.look_at(target, Vector3.UP)
